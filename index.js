c0e57554929f89b264a54a671c631cd61ea86b7f
vouneo <vouneo.dev@gmail.com>
2024-03-25 21:48:47 -0300

    agregando cliente spotify-spi

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 require("dotenv").config({ path: __dirname + "/.env" });
+const CronJob = require("cron").CronJob;
 
 const { twitterClient } = require("./twitterClient.js");
 
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
 const tweet = async () => {
     try {
-        await twitterClient.v2.tweet("Hola BM!");
+        await twitterClient.v2.tweet("hola a todo");
     } catch (e) {
         console.log(e)
     }
 }
 
-tweet();
+const cronTweet = new CronJob("30 7 * * *", async() => {
+    tweet();
+})
+
+cronTweet.start();
